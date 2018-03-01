5277dee270da4f62436b25eacb07ebd62337f236
Magic <sai.abhishek.2008@gmail.com>
2018-03-01 20:38:09 +0530

    Safety Feature
    Added a safety feature against peepers

--- ac/core/views.py
+++ ac/core/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import Http404, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 # Create your views here.
 def events(request):
@@ -7,6 +11,7 @@ def events(request):
 def gallery(request):
     return render(request, 'gallery.html',{})
  
+@login_required(redirect_field_name='loginpage')
 def index(request):
     return render(request, 'index.html',{})
 
@@ -16,8 +21,24 @@ def maps(request):
 def mapsd(request):
     return render(request,'3dmap.html',{})
 
-def login(request):
-    pass
+def loginpage(request):
+    return render(request,'login.html',{})
+
+def loginvalidate(request):
+    if request.method == 'POST':
+        user = request.POST.get('user')
+        password = request.POST.get('password')
+        user = authenticate(username = user, password = password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            error_message = 'invalid credentials'
+            return render(request,'error.html',{'error_message':error_message})
+    else:
+        error_message = 'This is not a valid request'
+        return render(request,'error.html',{'error_message':error_message})
+
 
 def signin(request):
     pass
