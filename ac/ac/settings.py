f357e85fa3b0823b5816ba26d1ff650245db0076
Magic <sai.abhishek.2008@gmail.com>
2018-03-03 20:06:30 +0530

    made changes for online service
    Changed settings.py for compatibility with pythonanywhere

--- ac/ac/settings.py
+++ ac/ac/settings.py
@@ -122,6 +122,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+#
+# STATIC_URL = '/static/'
+#
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'core','static'),
+# )
+#
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 STATIC_URL = '/static/'
 
@@ -129,4 +138,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'core','static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = "/home/acumenit/Nemuca-/ac/core/static/"
