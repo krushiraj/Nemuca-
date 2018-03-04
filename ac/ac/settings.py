dbce99b136fb498b373790aebfc8d6ea2fe730e1
Magic <sai.abhishek.2008@gmail.com>
2018-03-04 23:19:58 +0530

    Added Hosting Changes
    Changed Database settings , commented previous version for testing

--- ac/ac/settings.py
+++ ac/ac/settings.py
@@ -74,25 +74,25 @@ WSGI_APPLICATION = 'ac.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-"""
+# Hosting Version
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '',
+        'USER': 'super',
+        'PASSWORD': 'qwertyuiop',
+        'HOST': 'acumenit-690.postgres.pythonanywhere-services.com',
+        'PORT': '10690',
     }
 }
-"""
+#Testing Purupose
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
