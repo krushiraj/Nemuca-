d2ef9e946a2aa4d4c465e22f7a9a3a0bb78ab718
krushiraj <krushiraj123@gmail.com>
2018-03-04 17:49:14 +0530

    vv

--- ac/ac/settings.py
+++ ac/ac/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'ac.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -85,8 +85,14 @@ DATABASES = {
         'PORT': '',
     }
 }
+"""
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
