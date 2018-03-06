688e6acec5b0ca6f7e2186b3c76df0ab3ff3ad97
Magic <sai.abhishek.2008@gmail.com>
2018-03-06 17:57:49 +0530

    Corrected Android URLS

--- ac/android/urls.py
+++ ac/android/urls.py
@@ -1,6 +1,6 @@
 # from django.urls import path, include
 # from . import views
 
-# urlpatterns= [ 
-#     path('Event/',EventsView.as_view(), name = 'EventsView'),
-# ]
+urlpatterns= [ 
+    #path('Event/',EventsView.as_view(), name = 'EventsView'),
+]
