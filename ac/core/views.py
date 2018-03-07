9af5812795faa35a73e346522abdb95cfc487010
krushiraj <krushiraj123@gmail.com>
2018-03-07 13:38:04 +0530

    views with login

--- ac/core/views.py
+++ ac/core/views.py
@@ -8,7 +8,6 @@ from django.urls import reverse
 @login_required(redirect_field_name='loginpage')
 def events(request):
     return render(request, 'eventre.html',{})
-
 @login_required(redirect_field_name='loginpage')
 def gallery(request):
     return render(request, 'gallery.html',{})
@@ -16,15 +15,12 @@ def gallery(request):
 @login_required(redirect_field_name='loginpage')
 def index(request):
     return render(request, 'index.html',{})
-
 @login_required(redirect_field_name='loginpage')
 def home(request):
     return render(request,'home.html',{})
-
 @login_required(redirect_field_name='loginpage')
 def maps(request):
     return render(request,'map.html',{})
-
 @login_required(redirect_field_name='loginpage')
 def mapsd(request):
     return render(request,'3dmap.html',{})
@@ -47,25 +43,20 @@ def loginvalidate(request):
         error_message = 'This is not a valid request'
         return render(request,'error.html',{'error_message':error_message})
 
+def secret(request):
+    pass
 
 def signin(request):
     pass
-
 @login_required(redirect_field_name='loginpage')
 def signup(request):
      return render(request, 'registration.html',{})
-
 @login_required(redirect_field_name='loginpage')
 def social(request):
     return render(request, 'social.html',{})
-
 @login_required(redirect_field_name='loginpage')
 def sponsors(request):
     return render(request, 'sponsors.html',{})
-
 @login_required(redirect_field_name='loginpage')
 def team(request):
     return render(request, 'team.html',{})
-
-def secret(request):
-    return render(request,'ll.html' ,{})
