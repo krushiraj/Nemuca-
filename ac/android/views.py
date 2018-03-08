2c5e0018a7f7944e8a50f8f10230407243555d1c
Magic <sai.abhishek.2008@gmail.com>
2018-03-08 14:11:23 +0530

    conflict resolve

--- ac/android/views.py
+++ ac/android/views.py
@@ -186,19 +186,13 @@ def validateGame(eId,qID):
 def getUserEvent(request):
     message = 'Err'
     if request.method == 'POST':
-        # Fetch Registrations and participations for paid registered and participated
-<<<<<<< HEAD
-        profile = Profile.objects.get(QId= request.POST.get('qID'))
+        profile = Profile.objects.get(QId= request.POST['qID'])
         kp = profile.pk
-        query = RegistrationsAndParticipations.objects.filter(QId = kp )
-=======
-        query = RegistrationsAndParticipations.objects.filter( QId = request.POST.get('qId'))
-        print(query)
->>>>>>> 49271f71a4ffc7ef3af9707e3e841955abc22154
+        query = RegistrationsAndParticipations.objects.filter(QId = kp)
         if not query:
             return HttpResponse("Nope",content_type="text/plain")
         #Need to remove participated column
-        
+
         json_data = serializers.serialize('json',query)
         return HttpResponse(json_data,content_type = "application/json")
     else:
