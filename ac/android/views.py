55ad11b936995fe3a96c07decaee77edb0307945
Abhijith Chandra <abhijithchndr8@gmail.com>
2018-03-09 13:13:39 +0530

    Fixed issues with oneToone relation :|

--- ac/android/views.py
+++ ac/android/views.py
@@ -176,10 +176,10 @@ def validateGame(eId,qId):
     profile = Profile.objects.get(QId= qId)
     kp = profile.pk
     check = RegistrationsAndParticipations.objects.get(QId = kp)
-
+    eventId = Event.objects.get(eId=eId)
     #Check if user elgible ie. paid and not participated and registered
-    if eId in check.paid and eId in check.registered:
-        if eId not in check.participated:
+    if eventId in check.paid and eventId in check.registered:
+        if eventId not in check.participated:
             flag = True
 
     return flag
