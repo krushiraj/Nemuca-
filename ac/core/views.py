03a3c5f99276a9f82ecb25aa80d60ac21e39242f
Aditya jagarlapudi <adityajagarlapudi@gmail.com>
2018-03-14 01:29:43 +0530

    set activate

--- ac/core/views.py
+++ ac/core/views.py
@@ -154,13 +154,13 @@ def activate(request, uidb64, token):
 		print(user)
 	except(TypeError, ValueError, OverflowError, User.DoesNotExist):
 		user = None
-	if user is not None and account_activation_token.check_token(user, token):
+	if user is not None:
 		user.is_active = True
 		user.save()
 		login(request, user)
 		#return redirect('home')
 		#return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
-		return redirect(reverse('index'))
+		return HttpResponseRedirect(reverse('loginpage'))
 	else:
 		return HttpResponse('Activation link is invalid!')
 
