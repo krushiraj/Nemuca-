99a5b93307344bcf6c4fb07dfdcedc04cf7b78c2
Magic <sai.abhishek.2008@gmail.com>
2018-03-13 20:46:53 +0530

    removed extra fields

--- Nemuca--b65a2576019f86f128375029936e2645556621dc/ac/android/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AndroidConfig(AppConfig):
-    name = 'android'
--- Nemuca--b65a2576019f86f128375029936e2645556621dc/ac/android/serializers.py
+++
@@ -1,7 +0,0 @@
-# from rest_framework.serializers import ModelSerializer
-# from core.models import Events
-
-# class EventsSerializer(ModelSerializer):
-#     class Meta:
-#         model = Events
-#         fields = ['*']
