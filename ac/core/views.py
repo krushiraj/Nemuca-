ef3e01ea5b63d34057c5d8a922e926a0473e3abc
Aditya jagarlapudi <adityajagarlapudi@gmail.com>
2018-03-14 02:26:10 +0530

    change views

--- ac/core/views.py
+++ ac/core/views.py
@@ -92,13 +92,12 @@ def signup(request):
 			#print (form.data['username'])
 			user = User(username = request.POST.get('email'), password = request.POST.get('password'))
 			qrcode = get_random_string(5).lower()
-"""
 			user.is_active = False
 			current_site = get_current_site(request)
 			domain = current_site.domain
 			uid = urlsafe_base64_encode(force_bytes(user.pk))
 			uid1 = force_text(urlsafe_base64_decode(uid))
-			token = account_activation_token.make_token(user)"""
+			token = account_activation_token.make_token(user)
 			sample = pyq.create(qrcode)
 			# print(sample)
 			sample.png(qrcode+'.png',scale = 6)
@@ -117,7 +116,7 @@ def signup(request):
 			message = render_to_string('acc_active_email.html', {
 				'qrcode' : qrcode
 			})
-			#print(str('http://'+ "www.acumenit.in" +"/" +"activate" + "/" + str(uid.decode('utf-8')) + "/" + str(token)))
+			print(str('http://'+ "www.acumenit.in" +"/" +"activate" + "/" + str(uid.decode('utf-8')) + "/" + str(token)))
 			email = EmailMessage(
 						mail_subject, message, to=[emailid]
 			)
