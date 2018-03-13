be48a9091f9045a1c739e71e466f39b93e81f5b6
Aditya jagarlapudi <adityajagarlapudi@gmail.com>
2018-03-13 23:29:29 +0530

    change email

--- ac/ac/settings.py
+++ ac/ac/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'acumenit2k18@gmail.com'
+EMAIL_HOST_USER = 'acumenit2018@gmail.com'
 EMAIL_HOST_PASSWORD = 'itfest2018'
 EMAIL_PORT = 587
 # Application definition
